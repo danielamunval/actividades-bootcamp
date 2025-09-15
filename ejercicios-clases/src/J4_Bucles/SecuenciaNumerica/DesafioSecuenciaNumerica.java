4d9cbb02abaf5f437d32ee5ab10dd7dfc7574d7c
Daniela Muñoz Valdés <daniela.constanza03@gmail.com>
2025-09-15 01:34:02 -0300

    actualización ejercicio bucles

--- ejercicios-clases/src/J4_Bucles/SecuenciaNumerica/DesafioSecuenciaNumerica.java
+++ ejercicios-clases/src/J4_Bucles/SecuenciaNumerica/DesafioSecuenciaNumerica.java
@@ -1,18 +1,6 @@
 package J4_Bucles.SecuenciaNumerica;
 
 public class DesafioSecuenciaNumerica {
-    /**
-     * ACTIVIDAD:
-     * Desarrolla un programa en Java que imprima la siguiente secuencia numérica:
-     * 10, 20, 30, 40, 50, 60, 70, 80, 90 y 100
-     * Debes implementar la solución de tres maneras diferentes:
-     * 1. Utilizando un bucle for
-     * 2. Utilizando un bucle while
-     * 3. Utilizando un bucle do-while
-     * Observación importante:
-     * El último número (100) debe estar precedido por la conjunción "y" en lugar de una coma (,).
-     * El formato de salida debe ser exactamente como se muestra en el ejemplo.
-     **/
     public static void main(String[] args) {
 
         System.out.println("Secuencia numérica usando bucle 'for': ");
---
+++ ejercicios-clases/src/J4_Bucles/SecuenciaNumerica/README.md
@@ -0,0 +1,14 @@
+### ⌨️ Actividad Secuencia Numérica:
+
+### **Instrucciones para el Ejercicio "SecuenciaNumerica":**
+
+1. Desarrolla un programa en Java que imprima la siguiente **`secuencia numérica`**:
+                "10, 20, 30, 40, 50, 60, 70, 80, 90 y 100"
+
+2. Debes implementar la solución de tres maneras diferentes:
+   * Utilizando un bucle for
+   * Utilizando un bucle while
+   * Utilizando un bucle do-while
+3. Observación importante:
+   * El último número (100) debe estar precedido por la conjunción "y" en lugar de una coma (,).
+   * El formato de salida debe ser exactamente como se muestra en el ejemplo.
